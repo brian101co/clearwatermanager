06a7bd975714e71740798ba5f02d90f26ddeb618
Brian <semperindoctrina19@gmail.com>
2021-10-11 23:18:11 -0500

    removed event url

--- api/urls.py
+++ api/urls.py
@@ -4,6 +4,5 @@ from . import views
 urlpatterns = [
     path('reservations/', views.getTotalReservations),
     path('reservation/<int:id>/', views.getReservation, name="reservation"),
-    path('events/', views.event, name="events"),
     path('notifications/', views.getNotifications, name="notifications"),
 ]
