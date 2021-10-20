3963f5fb0b6b2c3f8eb8ec031734b3c10e6fbe53
Brian <semperindoctrina19@gmail.com>
2021-10-20 18:56:51 -0500

    added completed workorder detail and list views and templates

--- workorder/urls.py
+++ workorder/urls.py
@@ -5,14 +5,18 @@ from .views import (
     WorkorderDetailView,
     workorder_completed_view,
     WorkorderUpdateView,
-    WorkorderDeleteView
+    WorkorderDeleteView,
+    CompletedWorkorderListView,
+    CompletedWorkorderDetailView
 )
 
 urlpatterns = [
     path("", WorkorderListView.as_view(), name="workorder-list"),
     path("<int:id>/", WorkorderDetailView.as_view(), name="workorder-detail"),
     path("create/", WorkorderCreateView.as_view(), name="workorder-create"),
+    path("completed/", CompletedWorkorderListView.as_view(), name="workorder-completed"),
     path("edit/<int:id>/", WorkorderUpdateView.as_view(), name="workorder-update"),
     path("completed/<int:id>/", workorder_completed_view, name="workorder-completed"),
     path("delete/<int:id>/", WorkorderDeleteView.as_view(), name="workorder-delete"),
+    path("completed/<int:id>/detail/", CompletedWorkorderDetailView.as_view(), name="workorder-completed-detail"),
 ]
--- workorder/views.py
+++ workorder/views.py
@@ -36,6 +36,12 @@ class WorkorderListView(LoginRequiredMixin, ListView):
     ordering = "created_at"
     template_name = "workorders/list_workorders.html"
 
+class CompletedWorkorderListView(LoginRequiredMixin, ListView):
+    context_object_name = "workorders"
+    queryset = WorkOrder.objects.filter(completed=True)
+    ordering = "created_at"
+    template_name = "workorders/list_completed_workorders.html"
+
 class WorkorderCreateView(LoginRequiredMixin, CreateView):
     form_class = WorkorderForm
     success_url = reverse_lazy("workorder-list")
@@ -51,3 +57,8 @@ class WorkorderDetailView(LoginRequiredMixin, DetailView):
     model = WorkOrder
     pk_url_kwarg = 'id'
     template_name= "workorders/workorder_detail.html"
+
+class CompletedWorkorderDetailView(LoginRequiredMixin, DetailView):
+    queryset = WorkOrder.objects.filter(completed=True)
+    pk_url_kwarg = 'id'
+    template_name= "workorders/completed_workorder_detail.html"
