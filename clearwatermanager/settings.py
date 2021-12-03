ade758dd2d96becb140bd9d6c6b643bfb7f510c4
Brian <semperindoctrina19@gmail.com>
2021-12-02 18:49:40 -0600

    removing unused code

--- clearwatermanager/settings.py
+++ clearwatermanager/settings.py
@@ -140,9 +140,7 @@ STATICFILES_DIR = [
 TWILIO_ACCOUNT_SID = os.getenv("TWILIO_ACCOUNT_SID")
 TWILIO_AUTH_TOKEN = os.getenv("TWILIO_AUTH_TOKEN")
 TWILIO_NUMBER = os.getenv("TWILIO_NUMBER")
-SMS_BROADCAST_TO_NUMBERS = [
-    "+16015900576",
-]
+
 
 try:
     from .local_settings import *
