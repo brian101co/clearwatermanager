ac84b2fb6484c419d9e000a8f7bae5107c3b37c5
Brian <semperindoctrina19@gmail.com>
2021-12-02 18:48:30 -0600

    added django debug toolbar for profiling SQL queries

--- clearwatermanager/settings.py
+++ clearwatermanager/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'sites',
     'workorder',
     'crispy_forms',
+    "debug_toolbar",
 ]
 
 MIDDLEWARE = [
@@ -51,6 +52,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "debug_toolbar.middleware.DebugToolbarMiddleware",
 ]
 
 ROOT_URLCONF = 'clearwatermanager.urls'
--- clearwatermanager/urls.py
+++ clearwatermanager/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
 from sites import views as site_views
+from .settings import DEBUG
 
 urlpatterns = [
     path('', include("manager.urls")),
@@ -9,3 +10,9 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('site/info/<str:site>', site_views.get_site_info, name="site_info"),
 ]
+
+if DEBUG:
+    import debug_toolbar
+    urlpatterns += [
+        path('__debug__/', include(debug_toolbar.urls)),
+    ]
