d6b7a475616c64aea79d43e01d98a96cf7ace1d1
Brian <semperindoctrina19@gmail.com>
2026-06-06 12:57:00 -0500

    Add Site form and CRUD views with amenity fields and workorder context

---
+++ sites/forms.py
@@ -0,0 +1,56 @@
+from django import forms
+from .models import Site
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Layout, Field, Submit, HTML
+
+class SiteForm(forms.ModelForm):
+    class Meta:
+        model = Site
+        fields = [
+            'identifier', 'info', 'lot_type', 'under_maintenance',
+            'water', 'electric_30amp', 'electric_50amp', 'sewer',
+            'wifi', 'max_length_ft', 'nightly_rate', 'notes'
+        ]
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields["identifier"].label = "Lot Number"
+        self.fields["info"].label = "Information"
+        self.fields["lot_type"].label = "Lot Type"
+        self.fields["under_maintenance"].label = "Under Maintenance"
+        self.fields["water"].label = "Water Hookup"
+        self.fields["electric_30amp"].label = "30 AMP"
+        self.fields["electric_50amp"].label = "50 AMP"
+        self.fields["sewer"].label = "Sewer Hookup"
+        self.fields["wifi"].label = "WiFi"
+        self.fields["max_length_ft"].label = "Max Length (ft)"
+        self.fields["max_length_ft"].help_text = "Maximum RV length this lot can accommodate"
+        self.fields["nightly_rate"].label = "Nightly Rate"
+        self.fields["nightly_rate"].help_text = "Leave blank if rate varies"
+        self.fields["notes"].label = "Notes"
+        
+        self.helper = FormHelper()
+        self.helper.layout = Layout(
+            # Basic Info
+            HTML('<h6 class="text-muted mt-3 mb-2">Basic Information</h6>'),
+            Field("identifier", placeholder="e.g. 12, 10C"),
+            Field("lot_type"),
+            Field("info"),
+            
+            # Amenities
+            HTML('<h6 class="text-muted mt-3 mb-2">Amenities</h6>'),
+            Field("water"),
+            Field("sewer"),
+            Field("wifi"),
+            Field("electric_30amp"),
+            Field("electric_50amp"),
+            
+            # Details
+            HTML('<h6 class="text-muted mt-3 mb-2">Details</h6>'),
+            Field("max_length_ft", placeholder="e.g. 40"),
+            Field("nightly_rate", placeholder="00.00"),
+            Field("under_maintenance"),
+            Field("notes"),
+
+            Submit("submit", "Submit", css_class="btn pure-material-button-contained")
+        )
--- sites/views.py
+++ sites/views.py
@@ -1,8 +1,73 @@
 import json
+
 from django.shortcuts import render
+from django.contrib import messages
 from django.http import JsonResponse, HttpResponse
 from .models import Site
+from .forms import SiteForm
 from workorder.models import WorkOrder
+from django.urls import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import (
+    DetailView, 
+    DeleteView, 
+    CreateView,
+    UpdateView,
+    ListView
+)
+
+
+class SiteListView(LoginRequiredMixin, ListView):
+    model = Site
+    context_object_name = "sites"
+    template_name = "sites/list_sites.html"
+    ordering = ["identifier"]
+
+
+class SiteDetailView(LoginRequiredMixin, DetailView):
+    model = Site
+    pk_url_kwarg = "id"
+    context_object_name = "site"
+    template_name = "sites/site_detail.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["workorders"] = self.object.workorders.filter(completed=False)
+        return context
+
+
+class DeleteSiteView(LoginRequiredMixin, DeleteView):
+    model = Site
+    pk_url_kwarg = "id"
+    success_url = reverse_lazy('home')
+    template_name = "sites/delete_site.html"
+
+    def delete(self, request, *args, **kwargs):
+        messages.success(request, f'Site {self.get_object().identifier} has been deleted.')
+        return super().delete(request, *args, **kwargs)
+
+
+class EditSiteView(LoginRequiredMixin, UpdateView):
+    model = Site
+    form_class = SiteForm
+    pk_url_kwarg = "id"
+    success_url = reverse_lazy('home')
+    template_name = "sites/edit_site.html"
+
+    def form_valid(self, form):
+        messages.success(self.request, f'Site {form.instance.identifier} has been updated.')
+        return super().form_valid(form)
+
+
+class CreateSiteView(LoginRequiredMixin, CreateView):
+    form_class = SiteForm
+    success_url = reverse_lazy("home")
+    template_name = "sites/create_site.html"
+
+    def form_valid(self, form):
+        messages.success(self.request, f'Site {form.instance.identifier} has been created.')
+        return super().form_valid(form)
+
 
 def get_site_info(request, site):
     if request.method == "POST":
